51ae09e14377896d07df87dd94eb5eb1e295a3c9
Bilanox1 <aurabilanox@gmail.com>
2025-01-21 14:03:34 +0100

    Update dataBase.module.ts

--- src/Database/dataBase.module.ts
+++ src/Database/dataBase.module.ts
@@ -8,7 +8,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
     MongooseModule.forRootAsync({
       imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('DATABASE_URL'),
+        uri: "mongodb+srv://aurabilanox:04QmfgyrOMojJNcz@cluster0.1yf5r.mongodb.net/moride?retryWrites=true&w=majority&appName=Cluster0",
       }),
       inject: [ConfigService],
     }),
