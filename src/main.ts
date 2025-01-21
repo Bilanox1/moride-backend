947ab4c687738709df2f0a683070b06f39c54064
Bilanox1 <aurabilanox@gmail.com>
2025-01-21 14:17:02 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'https://accounts.google.com/'],
+    origin: "*",
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true, // This should be true only if cookies or credentials are sent
